cb095d30d3d9ac62cfe848f9274291b212a157d4
miloszlesiak <79806683+miloszlesiak@users.noreply.github.com>
2022-10-22 17:54:06 +0200

    Add files via upload

---
+++ zadanie1/app/calc.php
@@ -0,0 +1,42 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+$kwota_kredytu = $_REQUEST ['kwota_kredytu'];
+$il_rat = $_REQUEST ['il_rat'];
+$oprocentowanie = $_REQUEST ['oprocentowanie'];
+
+
+if ( ! (isset($kwota_kredytu) && isset($il_rat) && isset($oprocentowanie))) {
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+if ( $kwota_kredytu == "") {
+	$messages [] = 'Nie podano liczby 1';
+}
+if ( $il_rat == "") {
+	$messages [] = 'Nie podano liczby 2';
+}
+
+if (empty( $messages )) {
+	
+	if (! is_numeric( $kwota_kredytu )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $il_rat )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+if (empty ( $messages )) {
+	$odsetki = '';
+	$odsetki = floatval($odsetki);
+	$kwota_kredytu = intval($kwota_kredytu);
+	$il_rat = intval($il_rat);
+	$oprocentowanie = floatval($oprocentowanie);
+	
+	$odsetki = ($kwota_kredytu*(($oprocentowanie/$kwota_kredytu)*100));
+	$result = (($kwota_kredytu/$il_rat)+($odsetki))*$il_rat;
+}
+include 'calc_view.php';
---
+++ zadanie1/app/calc_view.php
@@ -0,0 +1,41 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator kredytowy</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<?php $kwota_kredytu='';$il_rat='';$oprocentowanie='';$il_rat_w_roku=''; ?>
+	<label for="id_kwota_kredytu">Kwota kredytu: </label>
+	<input id="id_kwota_kredytu" type="text" name="kwota_kredytu" value="<?php print($kwota_kredytu); ?>" /><br />
+	<label for="id_il_rat">Ilosc rat: </label>
+	<input id="id_il_rat" type="text" name="il_rat" value="<?php print($il_rat); ?>" /><br />
+	<label for="id_oprocentowanie">Oprocentowanie: </label>
+	<input id="id_oprocentowanie" type="text" name="oprocentowanie" value="<?php print($oprocentowanie); ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
